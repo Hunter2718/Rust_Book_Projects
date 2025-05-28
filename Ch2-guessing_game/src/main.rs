a95a95d4eaff42a9c94f249feede57379875f359
Hunter2718 <benmet2.7@gmail.com>
2025-05-28 15:58:22 -0500

    Add: Ch2 project (guessing game)

---
+++ Ch2-guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    print!("Guessing Game!\n");
+
+    let goal_number = rand::rng().random_range(1..=100);
+
+    loop {
+        print!("Enter Your Guess:\n");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&goal_number) {
+            Ordering::Less => print!("Too small\n"),
+            Ordering::Equal => {
+                print!("Correct\n");
+                break;
+            }
+            Ordering::Greater => print!("Too big\n"),
+        }
+    }
+}
